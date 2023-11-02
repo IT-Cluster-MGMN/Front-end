0afdc70120e84f5778e6065cbc15d19d48e920b1
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2023-11-02 22:02:12 +0200

    added_footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./Registration/Registration"
 import Login from "./Login/Login"
 
+import './App.css'
+
 function App() {
 
   return (
     <>
-      
-      <BrowserRouter>
-        <Routes>
-          <Route path="/reg" element={<Registration/>} />
-          <Route path="/log" element={<Login/>} />
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/reg" element={<Registration/>} />
+            <Route path="/log" element={<Login/>} />
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <footer className="footer"> {/* Apply the "footer" class */}
+        <p>Footer</p>
+      </footer>
     </>
   )
 }
