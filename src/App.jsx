66e66e499f85483e72c68312c9f170e973634ecb
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2023-11-10 20:14:43 +0200

    password_recovery_page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./Authentication/Registration"
 import Login from "./Authentication/Login"
 import Home from "./Home/Home"
+import ResetPassword from "./Authentication/ResetPassword";
 
 import './App.css'
 
@@ -16,6 +17,7 @@ function App() {
             <Route path="/registration" element={<Registration/>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/" element={<Home/>} />
+            <Route path="/resetpassword" element={<ResetPassword/>} />
           </Routes>
         </BrowserRouter>
       </div>
