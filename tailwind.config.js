07dca2eaad4da5a397e49d71b43fcf5a112e41e8
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2023-11-29 19:57:10 +0200

    tw_config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#fff",
+        gray: {
+          "100": "rgba(255, 255, 255, 0.8)",
+          "200": "rgba(12, 12, 13, 0.9)",
+        },
+        black: "#000",
+      },
+      spacing: {},
+      fontFamily: {
+        inika: "Inika",
+        inter: "Inter",
+      },
+    },
+    fontSize: {
+      base: "16px",
+      inherit: "inherit",
+    },
   },
-  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 }
