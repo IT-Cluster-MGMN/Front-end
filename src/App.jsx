811a70151a79c3ffc75ad3680ba4ab596709e2bb
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2023-12-15 16:33:35 +0200

    *added productpage, cart, profile *added basic functionality to product page *added route to product page in react router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Registration from "./components/Registration"
 import Login from "./components/Login"
 import Home from "./components/Home"
 import ResetPassword from "./components/ResetPassword";
+import ProductPage from "./components/ProductPage";
 
 import './App.css'
 
@@ -18,6 +19,7 @@ function App() {
             <Route path="/login" element={<Login/>} />
             <Route path="/" element={<Home/>} />
             <Route path="/resetpassword" element={<ResetPassword/>} />
+            <Route path="/products/:productId" element={<ProductPage/>} />
           </Routes>
         </BrowserRouter>
       </div>
---
+++ src/components/ProductPage.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+const API_ENDPOINT = "";
+
+const ProductPage = () => {
+  const { productId } = useParams();
+
+  const [product, setProduct] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${API_ENDPOINT}/products/${productId}`)
+      .then(res => setProduct(res.data))
+      .catch(error => console.error('Error fetching product data:', error));
+  }, [productId]);
+
+  const handleAddToCart = () => {
+    console.log("add to cart");
+  };
+
+  return (
+    <>
+      {product ? (
+        <div>
+          {/* Product Images */}
+          <div>
+            {/* Image Selector */}
+            <div>
+              {product.productImages.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`Product ${index + 1}`}
+                  onClick={() => setSelectedImage(index)}
+                  className={index === selectedImage ? 'selected' : ''}
+                />
+              ))}
+            </div>
+            {/* Main Image */}
+            <div>
+              <img
+                src={product.productImages[selectedImage]}
+                alt={`Product ${selectedImage + 1}`}
+              />
+            </div>
+          </div>
+          {/* Product Info + Buttons */}
+          <div>
+            {/* Product Info */}
+            <div>
+              <p>{product.productName}</p>
+              <p>{product.productDescription}</p>
+              <p>${product.productPrice}</p>
+            </div>
+            {/* Buttons */}
+            <div>
+              <button onClick={handleAddToCart}>Add to Cart</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...WIP</p>
+      )}
+    </>
+  );
+};
+
+export default ProductPage;
