2e11b9ea8fdbfe800cbd0cd8a05f308fca24dbed
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-01-15 21:58:10 +0200

    footer-fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
     BsPersonCircle,BsFillPersonFill,BsFillGearFill,
     BsBoxArrowRight,BsFillKeyFill,BsFillPersonPlusFill,
@@ -10,17 +9,15 @@ import {
 const Footer = () => {
     return (
         <>
-            <div className = "footer">
-                <div className="relative bg-white w-full h-[1240px] overflow-hidden text-left text-base z-1 text-black font-inter"> 
-                    <div className="absolute top-[1180px] left-[0px]" style={{ backgroundColor: "#0C0C0DE5", opacity: 0.9, width: "100vw", height: "60px", overflow: "hidden", color: "white" }}> {/* ось тут відбувається зміна футеру*/}
-                    <BsTelegram className="absolute top-[14px] left-[1400px] w-8 h-8 object-cover" />
-                    <BsDiscord className="absolute top-[14px] left-[1460px] w-8 h-8 object-cover" />
-                        <div className="absolute top-[16px] left-[1218px] inline-block w-[147px] h-7">
-                        Зв’язатись з нами
-                        </div>
+            <div className="flex flex-row items-center justify-end w-full bg-darkgrey h-[200px]">
+                <div className="flex flex-row items-center gap-3 mx-10">
+                    <div className="font-sans text-white">
+                        Зв'язатись з нами
                     </div>
-                </div>  
-            </div> 
+                    <BsTelegram/>
+                    <BsDiscord/>
+                </div>
+            </div>
         </>
     );
 };
