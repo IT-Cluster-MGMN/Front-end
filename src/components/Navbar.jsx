75baebbd7050e2ecc6dcdf8526a55ed3b8845b8b
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-01-15 21:58:43 +0200

    changed navbar position 	changed navbar position from absolute to sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,124 +27,123 @@ const Navbar = () => {
 
     return(
         <>
-            <div className="flex flex-col">
-                <nav className="
-                bg-darkgrey
-                w-screen
-                absolute
-                top-[0px]
-                left-[0px]
+            <nav className="
+            bg-darkgrey
+            w-full
+            sticky
+            top-[0px]
+            left-[0px]
+            bg-opacity-100
+            ">
+                <div className="
+                flex
+                items-center
+                px-[1%]
+                py-[0.25%]
+                gap-[1%]
                 ">
-                    <div className="
-                    flex
-                    items-center
-                    px-[1%]
-                    py-[0.25%]
-                    gap-[1%]
-                    ">
-                        {/* Logo */}
-                        <div className="flex-1 w-fit h-fit">
-                            <img className="h-11 w-11" src="logo_placeholder.png" alt="logo"/>
-                        </div>
-                        {/* Home and catalogue */}
-                        <div className="flex flex-row w-full max-w-[25%] justify-evenly gap-[5%]">
-                            {/* Home button */}
-                            <Link className={`${GENERIC_BUTTON_BACKGROUND} no-underline self-center h-3`} to="/">
-                                <div className={`${GENERIC_TEXT} flex items-center gap-2`}>
-                                    Home
-                                </div>
-                            </Link>
-                            {/* Catalogue button */}
-                            <button className={`${GENERIC_BUTTON_BACKGROUND} self-center gap-2 bg-inherit border-none`}
-                            onClick={handleOpenCategoriesMenu}>
-                                <div className={`${GENERIC_TEXT} flex items-center gap-2 bg-inherit border-none`}>
-                                    Catalogue
-                                    <BsGrid3X3Gap/>
-                                </div>
-                            </button>
-                        </div>
-                        {/* Search bar */}
-                        <div 
-                        className="
-                        flex 
-                        flex-row 
-                        items-center 
-                        w-full
-                        bg-white 
-                        rounded-[7px] 
-                        px-2 
-                        pb-[3px]
-                        pt-2
-
-                        border-b-[3px]
-                        border-t-[0px]
-                        border-x-[0px]
-                        border-solid
-                        border-white
-                
-                        focus-within:border-b-[3px]
-                        focus-within:border-t-[0px]
-                        focus-within:border-x-[0px]
-                        focus-within:border-green-700
-                        focus-within:border-solid
-
-                        hover:border-b-[3px]
-                        hover:border-t-[0px]
-                        hover:border-x-[0px]
-                        hover:border-green-700
-                        hover:border-solid
-
-                        delay-300 
-                        duration-300 
-                        hover:delay-300 
-                        hover:duration-300
-                        focus-within:delay-300
-                        focus-within:delay-300
-                        "
-                        >
-                            <BsSearch className="px-2"/>
-                            <input className="
-                            bg-white 
-                            w-full 
-                            h-full
-                            border-none
-                            font-sans
-                            font-bold
-                            text-black
-                            text-[15px]
-                            rounded-[7px]
-                            !outline-none
-                            "
-                            type="text"
-                            placeholder="Search"/>
-                        </div>
-                        {/* Minor functional buttons */}
-                        <div className="flex flex-row items-center justify-evenly gap-[5%] py-2 w-full max-w-[20%]">
-                            <button className={`${MINOR_BUTTON_BACKGROUND}`}>
-                                <BsFillHeartFill style={{color:"white"}}/>
-                                <p className={GENERIC_TEXT}>Favourite</p>
-                            </button>
-                            <button className={`${MINOR_BUTTON_BACKGROUND}`}>
-                                <BsFillTagsFill style={{color:"white"}}/>
-                                <p className={GENERIC_TEXT}>Sell</p>
-                            </button>
-                        </div>
-                        {/* Profile icon */}
-                        <div className="flex-col w-[10%] grow:1">
-                            <button 
-                            className="flex bg-white border-none rounded-full w-11 h-11"
-                            onClick={handleOpenProfileDropdown}
-                            >
-                                <img className="self-stretch w-full opacity-100 hover:opacity-25" src="profile-svgrepo-com.svg" alt="Profile"/>
-                            </button>
-                        </div>
+                    {/* Logo */}
+                    <div className="flex-1 w-fit h-fit">
+                        <img className="h-11 w-11" src="logo_placeholder.png" alt="logo"/>
                     </div>
-                    {/* Dropdown menu */}
-                    {openProfileDropdown ? (<ProfileDropdown/>) : null}
-                    {/* Categories menu */}
-                    {openCategoriesMenu ? (<CategoriesMenu/>):null}
-                </nav>
-            </div>
+                    {/* Home and catalogue */}
+                    <div className="flex flex-row w-full max-w-[25%] justify-evenly gap-[5%]">
+                        {/* Home button */}
+                        <Link className={`${GENERIC_BUTTON_BACKGROUND} no-underline self-center max-h-3`} to="/">
+                            <div className={`${GENERIC_TEXT} flex items-center gap-2`}>
+                                Home
+                            </div>
+                        </Link>
+                        {/* Catalogue button */}
+                        <button className={`${GENERIC_BUTTON_BACKGROUND} self-center gap-2 bg-inherit border-none`}
+                        onClick={handleOpenCategoriesMenu}>
+                            <div className={`${GENERIC_TEXT} flex items-center gap-2 bg-inherit border-none`}>
+                                Catalogue
+                                <BsGrid3X3Gap/>
+                            </div>
+                        </button>
+                    </div>
+                    {/* Search bar */}
+                    <div 
+                    className="
+                    flex 
+                    flex-row 
+                    items-center 
+                    w-full
+                    bg-white 
+                    rounded-[7px] 
+                    px-2 
+                    pb-[3px]
+                    pt-2
+
+                    border-b-[3px]
+                    border-t-[0px]
+                    border-x-[0px]
+                    border-solid
+                    border-white
+            
+                    focus-within:border-b-[3px]
+                    focus-within:border-t-[0px]
+                    focus-within:border-x-[0px]
+                    focus-within:border-green-700
+                    focus-within:border-solid
+
+                    hover:border-b-[3px]
+                    hover:border-t-[0px]
+                    hover:border-x-[0px]
+                    hover:border-green-700
+                    hover:border-solid
+
+                    delay-300 
+                    duration-300 
+                    hover:delay-300 
+                    hover:duration-300
+                    focus-within:delay-300
+                    focus-within:delay-300
+                    "
+                    >
+                        <BsSearch className="px-2"/>
+                        <input className="
+                        bg-white 
+                        w-full 
+                        h-full
+                        border-none
+                        font-sans
+                        font-bold
+                        text-black
+                        text-[15px]
+                        rounded-[7px]
+                        !outline-none
+                        "
+                        type="text"
+                        placeholder="Search"/>
+                    </div>
+                    {/* Minor functional buttons */}
+                    <div className="flex flex-row items-center justify-evenly gap-[5%] py-2 w-full max-w-[20%]">
+                        <button className={`${MINOR_BUTTON_BACKGROUND}`}>
+                            <BsFillHeartFill style={{color:"white"}}/>
+                            <p className={GENERIC_TEXT}>Favourite</p>
+                        </button>
+                        <button className={`${MINOR_BUTTON_BACKGROUND}`}>
+                            <BsFillTagsFill style={{color:"white"}}/>
+                            <p className={GENERIC_TEXT}>Sell</p>
+                        </button>
+                    </div>
+                    {/* Profile icon */}
+                    <div className="flex-col w-[10%] grow:1">
+                        <button 
+                        className="flex bg-white border-none rounded-full w-11 h-11"
+                        onClick={handleOpenProfileDropdown}
+                        >
+                            <img className="self-stretch w-full opacity-100 hover:opacity-25" src="profile-svgrepo-com.svg" alt="Profile"/>
+                        </button>
+                    </div>
+                </div>
+                {/* Dropdown menu */}
+                {openProfileDropdown ? (<ProfileDropdown/>) : null}
+                {/* Categories menu */}
+                {openCategoriesMenu ? (<CategoriesMenu/>):null}
+            </nav>
         </>
     );
 };
