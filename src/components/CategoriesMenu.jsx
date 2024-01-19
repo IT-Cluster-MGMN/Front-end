73f4d49d54b42abd01423f6891eeb9045b182769
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-01-19 17:28:45 +0200

    Categories Menu change 	*added scrollbar 	*made scrollbar invisible 	*added urls to subcategories in CategoriesMenuItems.jsx 	*minor style changes

--- src/components/CategoriesMenu.jsx
+++ src/components/CategoriesMenu.jsx
@@ -5,24 +5,11 @@ import { categoriesData } from "./CategoriesMenuItems";
 
 const CategoriesMenu = () => {
     const CATEGORIES_MENU_TEXT = 'font-sans font-semibold text-black text-[10px] no-underline';
-    const CATEGORIES_MENU_BUTTONS = 'flex items-center justify-start w-full gap-2 px-5 py-2 border-none bg-inherit bg-zinc-300 hover:bg-zinc-400';
+    const CATEGORIES_MENU_BUTTONS = 'flex items-center justify-start w-full gap-2 px-5 py-2 border-none bg-zinc-300 m-[5px] hover:bg-zinc-400';
     
     const categories = categoriesData;
 
     const [category, setCategory] = useState(categories[0]);
-
-    
-
-    // const categories = ['electronics', 'autoAndParts', 'apparelAndAccessories', 'toysAndHobby', 'petSupplies', 'gardenEquipment']
-
-    // const categoriesMap = {
-    //     'electronics':{'title':'Electronics', 'icon':<BsPhone/>, 'subcategory':['1sub1', '1sub2', '1sub3']},
-    //     'autoAndParts':{'title':'Auto and Parts', 'icon':<BsPhone/>, 'subcategory':['2sub1', '2sub2', '2sub3']},
-    //     'apparelAndAccessories':{'title':'Apparel and accessories', 'icon':<BsPhone/>, 'subcategory':['3sub1', '3sub2', '3sub3']},
-    //     'toysAndHobby':{'title':'Toys and hobby', 'icon':<BsPhone/>, 'subcategory':['4sub1', '4sub2', '4sub3']},
-    //     'petSupplies':{'title':'Pet supplies', 'icon':<BsPhone/>, 'subcategory':['5sub1', '5sub2', '5sub3']},
-    //     'gardenEquipment':{'title':'Garden equipment', 'icon':<BsHouse/>, 'subcategory':['6sub1', '6sub2', '6sub3']}
-    // };
 
     const handleCategorySelection = (value) => {
         setCategory(value);
@@ -30,12 +17,12 @@ const CategoriesMenu = () => {
 
     return(
         <>
-            <div className="flex absolute justify-self-center left-[5%] bg-black w-[90%]">
+            <div className="flex flex-row absolute justify-self-center left-[3%] w-[94%] max-h-[300px]">
                 {/* Basic categories */}
-                <div className="flex flex-col w-[25%] bg-black">
+                <div className="flex flex-col items-start w-[25%] bg-zinc-300 rounded-bl-[10px] py-4 overflow-y-auto">
                     {categories.map(value => (
                         // arrow function in the onClick scope passes parameters to the handleCategorySelection
-                        <button className={CATEGORIES_MENU_BUTTONS} onClick={() => handleCategorySelection(value)}>
+                        <button className="flex w-full gap-2 px-5 my-2 border-none bg-inherit hover:underline hover:cursor-pointer" onClick={() => handleCategorySelection(value)}>
                             {value['icon']}
                             {value['title']}
                         </button>
@@ -43,12 +30,12 @@ const CategoriesMenu = () => {
                     )}
                 </div>
                 {/* Subcategories */}
-                <div className="grid w-full grid-cols-2 bg-zinc-200">
+                <div className="grid w-full grid-cols-3 bg-zinc-200 px-5 rounded-br-[10px] overflow-y-scroll">
                     {category['subcategories'].map(subcategory => (
-                        <div className="flex flex-col">
-                            <p className="">{subcategory['title']}</p>
+                        <div className="flex flex-col px-3 py-2">
+                            <p className="font-sans font-semibold text-[17px]">{subcategory['title']}</p>
                             {subcategory['subsubcategory'].map(subsubcategory => (
-                                <Link to={subsubcategory['url']}>{subsubcategory['title']}</Link>
+                                <Link className="font-sans text-[13px] text-black no-underline hover:underline" to={subsubcategory['url']}>{subsubcategory['title']}</Link>
                             ))}
                         </div>
                     ))}
--- src/components/CategoriesMenuItems.jsx
+++ src/components/CategoriesMenuItems.jsx
@@ -1,4 +1,5 @@
-import { BsCarFront, BsPhone } from "react-icons/bs";
+import { BsCarFront, BsPhone, BsSuitHeart } from "react-icons/bs";
+import { VscJersey } from "react-icons/vsc";
 
 export const categoriesData = [
     {
@@ -6,14 +7,42 @@ export const categoriesData = [
         id:'electronics',
         icon:<BsPhone/>,
         subcategories:[
-            {title:'1sub1', subsubcategory:[
+            {title:'1sub1', url:'/test', subsubcategory:[
                 {title:'11subsub1', url:'link'},
                 {title:'11subsub2', url:'link'}
             ]},
-            {title:'1sub2', subsubcategory:[
+            {title:'1sub2', url:'/test', subsubcategory:[
                 {title:'12subsub1', url:'link'},
                 {title:'12subsub2', url:'link'}
-            ]}
+            ]},
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
+            {title:'1sub2', url:'/test', subsubcategory:[
+                {title:'12subsub1', url:'link'},
+                {title:'12subsub2', url:'link'}
+            ]},
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
         ]
     },
     {
@@ -21,14 +50,74 @@ export const categoriesData = [
         id:'autoAndParts',
         icon:<BsCarFront/>,
         subcategories:[
-            {title:'2sub1', subsubcategory:[
+            {title:'2sub1', url:'/test', subsubcategory:[
                 {title:'21subsub1', url:'link'},
                 {title:'21subsub2', url:'link'}
             ]},
-            {title:'2sub2', subsubcategory:[
+            {title:'2sub2', url:'/test', subsubcategory:[
                 {title:'22subsub1', url:'link'},
                 {title:'22subsub2', url:'link'}
             ]}
         ]
-    }
+    },
+    {
+        title:'Clothes and Accessories',
+        id:'clothes',
+        icon:<VscJersey/>,
+        subcategories:[
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
+            {title:'1sub2', url:'/test', subsubcategory:[
+                {title:'12subsub1', url:'link'},
+                {title:'12subsub2', url:'link'}
+            ]}
+        ]
+    },
+    {
+        title:'Electronics',
+        id:'electronics',
+        icon:<BsPhone/>,
+        subcategories:[
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
+            {title:'1sub2', url:'/test', subsubcategory:[
+                {title:'12subsub1', url:'link'},
+                {title:'12subsub2', url:'link'}
+            ]}
+        ]
+    },
+    {
+        title:'Electronics',
+        id:'electronics',
+        icon:<BsPhone/>,
+        subcategories:[
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
+            {title:'1sub2', url:'/test', subsubcategory:[
+                {title:'12subsub1', url:'link'},
+                {title:'12subsub2', url:'link'}
+            ]}
+        ]
+    },
+    {
+        title:'Electronics',
+        id:'electronics',
+        icon:<BsPhone/>,
+        subcategories:[
+            {title:'1sub1', url:'/test', subsubcategory:[
+                {title:'11subsub1', url:'link'},
+                {title:'11subsub2', url:'link'}
+            ]},
+            {title:'1sub2', url:'/test', subsubcategory:[
+                {title:'12subsub1', url:'link'},
+                {title:'12subsub2', url:'link'}
+            ]}
+        ]
+    },
 ];
