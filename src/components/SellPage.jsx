baf2e6c3043f77f347724e262a0ace05c6cd760f
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-01-25 14:09:00 +0200

    Login-and-sellPage

---
+++ src/components/SellPage.jsx
@@ -0,0 +1,10 @@
+import Navbar from './Navbar'
+
+const SellPage = () => {
+    return(
+        <>
+            <div className='absolute top-0 left-0 w-full'><Navbar/></div>
+        </>
+    );
+};
+export default SellPage;
