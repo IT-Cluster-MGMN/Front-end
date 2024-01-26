2e500ebbaf0061c9ed7e80fb16c5f64ab5cb45ed
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-01-26 15:19:45 +0200

    Fixed footer height

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,13 @@
 import {
-    BsPersonCircle,BsFillPersonFill,BsFillGearFill,
-    BsBoxArrowRight,BsFillKeyFill,BsFillPersonPlusFill,
-    BsSearch,BsTelegram,BsDiscord,BsFillBasket2Fill,
-    BsFillHeartFill,BsList,BsFillTagsFill,BsPhone,BsTv,BsLaptop,
-    BsCarFront,BsFillUmbrellaFill,BsRocket, BsTencentQq, BsDribbble, BsFillHouseFill
-  } from "react-icons/bs";
+    
+    BsTelegram,BsDiscord
+    
+      } from "react-icons/bs";
 
 const Footer = () => {
     return (
         <>
-            <div className="flex flex-row items-center justify-end w-full bg-darkgrey h-[200px]">
+            <div className="flex flex-row items-center justify-end w-full bg-darkgrey h-[20rem]">
                 <div className="flex flex-row items-center gap-3 mx-10">
                     <div className="font-sans text-white">
                         Зв'язатись з нами
