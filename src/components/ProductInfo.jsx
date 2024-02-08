592e0a73f0a9da6ca5b87bc2329251b28f5cfd6a
NazariyBrynyarskiy <nbrynyarsky@gmail.com>
2024-02-08 14:18:23 +0200

    temp set default image for all products

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -6,13 +6,13 @@ const ProductInfo = ({item}) => {
     return(
         <>
             <Link className="card" to={`../product/${item.productId}`}>
-                <img src={`$'src/assets/not-found.png'}`} alt="image" className="card-image"/>
+                <img src={`/src/assets/not-found.png`} alt="image" className="card-image"/>
                 <span className="card-title">{item.title}</span>
                 {/* <span className="text-zinc-500 text-[0.8rem]">category name</span> */}
                 <span className="card-price">{item.price}</span>
                 <button className="add-to-cart">Add to Favourites</button>
                 {/* <span className="text-[0.8rem] line-clamp-3 break-all">{item.description}</span> */}
-                
+
             </Link>
         </>
     );
