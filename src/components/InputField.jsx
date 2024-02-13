d2cbd6828889035a7d93e4447f910ecc600310a4
kgeorgiy5 <kulikovskiy.g.05@gmail.com>
2024-02-13 16:10:30 +0200

    moved-input-field

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,11 +1,17 @@
-const InputField = ({label, onChange}) => {
-
-  return(
-    <>
-      <span>{label}</span>
-      <input onChange={(e) => onChange(e.target.value)}/>
-    </>
-  )
+const InputField = ({ label, onChange, className, placeholder }) => {
+  return (
+    <div className={`mb-4 ${className}`}>
+      <label className="block text-gray-700 text-sm font-bold mb-2 w-1/2">
+        {label}
+      </label>
+      <input
+        type="text"
+        onChange={onChange}
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        placeholder={placeholder}
+      />
+    </div>
+  );
 };
 
 export default InputField;
