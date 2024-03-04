81648d5cdf0b282accfa682e7822e8b3276750c6
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-03-04 18:20:04 +0200

    removed-profilepage-top-buttons

--- src/pages/ProfilePage/components/ProfileBox.jsx
+++ src/pages/ProfilePage/components/ProfileBox.jsx
@@ -16,20 +16,20 @@ const ProfileBox = () => {
     setIsEditting(!isEditting);
   };
 
+  const handleSubmit = () => {
+    window.location.reload(false);
+  };
+
   return (
     <>
       <div className="flex items-center flex-col h-full justify-center p-2 px-[15rem] gap-y-2">
-        <div className="flex items-center justify-around flex-row w-full p-2">
-          <button>Dashboard</button>
-          <button>Chats</button>
-          <button>Cart</button>
-        </div>
         <div className="bg-zinc-400 w-full h-full flex flex-row rounded-[3rem] p-[2rem] gap-[1rem]">
           {isEditting ? (
             <ProfileEdit
               user={user}
               contacts={contacts}
               editClick={handleEditButtonClick}
+              onSubmit={handleSubmit}
             />
           ) : (
             <ProfileInfo
