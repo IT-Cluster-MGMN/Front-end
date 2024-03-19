f9b0dba99dd77bc440b6a7b4d83ba127104b6128
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-03-19 08:39:22 +0200

    green-button-hover-fix

--- src/components/GreenButton.jsx
+++ src/components/GreenButton.jsx
@@ -2,7 +2,7 @@ const GreenButton = ({ onClick, children, hasHover }) => {
   return (
     <button
       onClick={onClick}
-      className={`p-2 rounded border-none bg-white text-green-600 ${hasHover ? "hover:bg-green-700 hover:text-white" : ""} text-[1rem] transition duration-200 font-sans font-bold `}
+      className={`p-2 rounded border-none bg-white text-green-600 ${hasHover ? "hover:bg-green-700 hover:text-white cursor-pointer" : ""} text-[1rem] transition duration-200 font-sans font-bold `}
     >
       {children}
     </button>
