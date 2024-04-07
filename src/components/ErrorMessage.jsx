158e0066066ef4071e8dc1946833907cc9617351
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-04-07 20:35:36 +0300

    error-message-hotfix

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -7,10 +7,12 @@ const ErrorMessage = ({ errorMsg, setErrorMsg }) => {
   return (
     <div
       onClick={() => setErrorMsg("")}
-      className={`${errorMsg ? "opacity-100" : "opacity-0"} delay-75 duration-300 absolute transition cursor-pointer flex w-[15rem] h-[4rem] overflow-hidden flex-row items-center gap-1 p-2 z-50 top-5 right-5 rounded bg-red-500   `}
+      className={`${errorMsg ? "opacity-100 w-[15rem] h-[4rem] top-5  bg-red-500" : "bg-transparen top-0  opacity-0 h-0 z-0 w-0"} right-5 duration-600 absolute transition-all cursor-pointer flex   overflow-hidden flex-row items-center gap-1 p-2 z-50  rounded    `}
     >
-      <IoIosAlert color="white" />
-      <span className="font-sans text-white">{errorMsg}</span>
+      {errorMsg ? <IoIosAlert color="white" /> : null}
+      {errorMsg ? (
+        <span className="font-sans text-white">{errorMsg}</span>
+      ) : null}
     </div>
   );
 };
