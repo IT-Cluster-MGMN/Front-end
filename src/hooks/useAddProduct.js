22be7aa66ace9cf38e46efae156713cd57f5d55f
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-04-07 20:25:26 +0300

    added-redirection

--- src/hooks/useAddProduct.js
+++ src/hooks/useAddProduct.js
@@ -1,26 +1,35 @@
-import requestWithCredentials from "../services/requestWithCredentials.js";
+import axios from "axios";
 
 const useAddProduct = async (data, images) => {
   const IMAGES_ENDPOINT = "http://localhost:8000/api/product/photo/add";
   const INFO_ENDPOINT = "http://localhost:8000/api/product/add";
 
-  requestWithCredentials.post(INFO_ENDPOINT, data).then((resInfo) => {
-    images.forEach((image) => {
-      fetch(image)
+  try {
+    const resInfo = await axios.post(INFO_ENDPOINT, data, {
+      withCredentials: true,
+    });
+
+    const imageUploadPromises = images.map((image) => {
+      return fetch(image)
         .then((res) => res.blob())
         .then((blob) => {
           const file = new File([blob], "File name", { type: "image/jpeg" });
           const formData = new FormData();
           formData.append("file", file, "image.jpg");
           formData.append("id", resInfo.data.productId);
-          requestWithCredentials
-            .post(IMAGES_ENDPOINT, formData, {
-              headers: { "Content-Type": "multipart/form-data" },
-            })
-            .catch((err) => console.log(err));
+          return axios.post(IMAGES_ENDPOINT, formData, {
+            headers: { "Content-Type": "multipart/form-data" },
+            withCredentials: true,
+          });
         });
     });
-  });
+
+    await Promise.all(imageUploadPromises);
+
+    window.location.href = "../";
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default useAddProduct;
