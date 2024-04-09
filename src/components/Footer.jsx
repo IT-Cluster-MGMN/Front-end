7e97ec3735fc2f151b463d0312d5f84242eea4ee
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-04-09 20:32:24 +0300

    footer-height-fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,13 @@
-import {
-    
-    BsTelegram,BsDiscord
-    
-      } from "react-icons/bs";
+import { BsTelegram, BsDiscord } from "react-icons/bs";
 
 const Footer = () => {
     return (
         <>
-            <div className="flex flex-row items-center justify-end w-full bg-darkgrey h-[20rem]">
-                <div className="flex flex-row items-center gap-3 mx-10">
-                    <div className="font-sans text-white">
-                        Зв'язатись з нами
-                    </div>
-                    <BsTelegram/>
-                    <BsDiscord/>
+            <div className="flex flex-row w-full min-h-[20rem] bg-darkgrey h-[20rem]">
+                <div className="flex flex-row items-center gap-3 h-full">
+                    <div className="font-sans text-white">Зв'язатись з нами</div>
+                    <BsTelegram />
+                    <BsDiscord />
                 </div>
             </div>
         </>
