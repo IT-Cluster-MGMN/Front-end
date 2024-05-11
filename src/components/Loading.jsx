36526eb6c22bfb4f20d0215219c240f8bd30a12b
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-05-11 23:04:13 +0300

    fixed-overlay-scroll

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,8 +3,8 @@ import { ImSpinner } from "react-icons/im";
 
 const Loading = ({ darkTheme }) => {
   return (
-    <div className="absolute w-screen h-screen overflow-hidden justify-center top-0 left-0 z-20 flex items-center bg-[rgba(0,0,0,0.5)]">
-      <div className="flex flex-row items-center gap-2">
+    <div className="absolute w-screen h-full overflow-hidden justify-center top-0 left-0 z-20 flex items-center bg-[rgba(0,0,0,0.5)]">
+      <div className="fixed top-[50%] flex flex-row justify-center w-screen items-center gap-2">
         <span
           className={`${darkTheme ? "text-white" : "text-black"} text-[1.5rem] font-sans font-bold`}
         >
--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -2,7 +2,7 @@ const Overlay = ({ onClick, zIndex = "z-0" }) => {
   return (
     <div
       onClick={onClick}
-      className={`absolute backdrop-blur-lg ${zIndex} w-screen h-screen bg-black  bg-opacity-70`}
+      className={`absolute backdrop-blur-lg ${zIndex} w-screen h-full min-h-screen bg-black  bg-opacity-70`}
     />
   );
 };
