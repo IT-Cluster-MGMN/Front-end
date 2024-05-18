fa8d5328065b775db85f1f40901b12168b44c925
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-05-18 23:45:28 +0300

    added-generate-description-button-styles

--- src/pages/SellPage/components/DescriptionField.jsx
+++ src/pages/SellPage/components/DescriptionField.jsx
@@ -41,7 +41,7 @@ const DescriptionField = ({ onChange, category, title, isValidTitle }) => {
 
   return (
     <>
-      <div className="flex gap-2 flex-col">
+      <div className="flex gap-4 flex-col">
         <div
           className={`${isAi ? "h-[3rem]" : "h-0"} transition-all w-full flex flex-col gap-2`}
         >
@@ -55,21 +55,32 @@ const DescriptionField = ({ onChange, category, title, isValidTitle }) => {
             onChange={(e) => setColor(e.target.value)}
           />
         </div>
-        <div className="flex flex-row items-center gap-x-2">
-          <label className="font-sans font-bold text-white">Опис</label>
-          <BsStars
-            onClick={handleAi}
-            className="text-white cursor-pointer hover:text-purple-500 transition p-1 rounded-full"
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-row items-center gap-x-2">
+            <label className="font-sans font-bold text-white">Опис</label>
+            <div
+              className={`p-1 overflow-hidden ${isAi ? "px-3 bg-zinc-300 w-fit text-purple-500 hover:text-white transition-all duration-300 hover:bg-gradient-to-r hover:from-purple-500 hover:via-indigo-400 hover:to-purple-500 cursor-pointer border-[1.5px] border-solid border-transparent hover:border-white" : "bg-inherit hover:text-purple-500 text-white w-[1rem]"} transition-all cursor-pointer gap-1 rounded-[1rem] items-center flex flex-row`}
+              onClick={handleAi}
+            >
+              <BsStars className=" cursor-pointer rounded-full" />
+              {isAi ? (
+                <>
+                  <label className="font-sans cursor-pointer  text-[0.9rem] font-semibold">
+                    Згенерувати опис
+                  </label>
+                </>
+              ) : null}
+            </div>
+          </div>
+          <textarea
+            onChange={(e) => handleChange(e.target.value)}
+            cols="30"
+            rows="7"
+            maxLength="2000"
+            className="resize-none rounded p-1 text-white bg-[rgba(50,50,50,1)] font-sans focus:outline-none"
+            value={description}
           />
         </div>
-        <textarea
-          onChange={(e) => handleChange(e.target.value)}
-          cols="30"
-          rows="7"
-          maxLength="2000"
-          className="resize-none rounded p-1 text-white bg-[rgba(50,50,50,1)] font-sans focus:outline-none"
-          value={description}
-        />
       </div>
       {errorMessage ? (
         <ErrorMessage errorMsg={errorMessage} setErrorMsg={setErrorMessage} />
