f8952ea379548a75e60c2b2f9b1601da909e115f
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-05-18 23:46:35 +0300

    footer-hotfix

--- src/pages/SellPage/SellPage.jsx
+++ src/pages/SellPage/SellPage.jsx
@@ -1,12 +1,16 @@
+import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import ProductBox from "./components/ProductBox";
 
 const SellPage = () => {
   return (
     <>
-      <div className="absolute top-0 left-0 h-screen w-screen overflow-hidden">
+      <div className="absolute top-0 left-0 h-screen w-screen overflow-x-hidden">
         <Navbar />
-        <ProductBox />
+        <div className="flex flex-col">
+          <ProductBox />
+          <Footer />
+        </div>
       </div>
     </>
   );
