fa33b52fe4e93dc7e9718145db691d2edaa0eef7
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-05-18 23:47:16 +0300

    added-className-arg-to-greenButton

--- src/components/GreenButton.jsx
+++ src/components/GreenButton.jsx
@@ -1,9 +1,15 @@
-const GreenButton = ({ onClick, children, hasHover, disabled = false }) => {
+const GreenButton = ({
+  className,
+  onClick,
+  children,
+  hasHover,
+  disabled = false,
+}) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`${disabled ? "text-zinc-500" : ""} select-none p-2 rounded border-none bg-white text-green-600 ${hasHover ? "hover:bg-green-700 hover:text-white cursor-pointer" : ""} text-[1rem] transition duration-200 font-sans font-bold `}
+      className={`${className} ${disabled ? "text-zinc-500" : ""} select-none p-2 rounded border-none bg-white text-green-600 ${hasHover ? "hover:bg-green-700 hover:text-white cursor-pointer" : ""} text-[1rem] transition duration-200 font-sans font-bold `}
     >
       {children}
     </button>
